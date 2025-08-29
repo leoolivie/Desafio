7c476f37f9595e838e108da8df0c3800b5755bb3
Leonardo Oliveira <116569223+leonardo-oliveira-bnw@users.noreply.github.com>
2025-08-29 16:23:03 -0300

    Arquivos do desafio

--- Desafio/DesafioApi/Models/Titulo.cs
+++ Desafio/DesafioApi/Models/Titulo.cs
@@ -9,9 +9,9 @@ public class Parcela
 
 public class Titulo
 {
-    public string NumeroTitulo { get; set; }
-    public string NomeDevedor { get; set; }
-    public string CpfDevedor { get; set; }
+    public string? NumeroTitulo { get; set; }
+    public string? NomeDevedor { get; set; }
+    public string? CpfDevedor { get; set; }
     public decimal PercentualJuros { get; set; }
     public decimal PercentualMulta { get; set; }
     public List<Parcela> Parcelas { get; set; } = new();
